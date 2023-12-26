c7a68575b507330d4c3426268dbd11f1e7b8dc69
Suarenguen <adrian.suarez.monzon@gmail.com>
2023-12-26 23:32:54 +0000

    bugs fixed and select added with filter

--- src/services/students.service.js
+++ src/services/students.service.js
@@ -3,14 +3,13 @@ import api from './config'
 
 const getAllStudents = async () => {
     try {
-        const { data } = await api.get('/students')
+        const { data } = await api.get('/students/studentsData')
         console.log(data)
         return data
     } catch (error) {
         throw new Error(error)
     }
 }
-
 
 export {
      getAllStudents
--- src/views/ListStudents.vue
+++ src/views/ListStudents.vue
@@ -2,14 +2,18 @@
     <div>
         <button>Refresh Data</button>
         <h1>List Students</h1>
-        <TableComponent :students="students"/>
+        <select name="" id="" v-model="select">
+            <option value="SCE1">SCE1</option>
+            <option value="SCE2">SCE2</option>
+        </select>
+        <TableComponent :students="students.students"/>
     </div>
 </template>
 
 <script>
 import TableComponent from '../components/TableComponent.vue'
 import { getAllStudents } from '../services/students.service'
-import { ref } from 'vue'
+import { ref, onMounted, computed } from 'vue'
     export default {
         name: "ListStudents",
         components: {
@@ -17,13 +21,22 @@ import { ref } from 'vue'
         },
         setup() {
             const students = ref([])
+            const select = ref('SCE1')
             const fetchStudenstData = async () => {
                 const response = await getAllStudents()
                 students.value = response
+                console.log(students.value)
             }
-            fetchStudenstData()
+            onMounted(fetchStudenstData)
+            const filteredStudents = computed(() => {
+            return students.value.students.filter(student => {
+                return student.courses.some(course => course.title === select.value);
+            });
+        });
             return {
-                students
+                students,
+                select,
+                filteredStudents
             }
             
         }
